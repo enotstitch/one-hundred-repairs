e318fd6b0d6e0b124a0f0e563839c7eacca3e7d8
ZayRexan <mogilevkostya@yandex.ru>
2023-04-25 00:15:39 +0700

    Подключение модальных окон

--- assets/js/_functions.js
+++ assets/js/_functions.js
@@ -1,5 +1,6 @@
 import { header } from '../components/header/header';
 import { select } from '../components/select/select';
+import { modal } from '../components/modal/modal';
 import { tabs } from '../components/tabs/tabs';
 import { swiper } from './functions/swiper-init';
 import { spollers } from '../components/spollers/spollers';

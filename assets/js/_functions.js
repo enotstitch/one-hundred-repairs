742a7850916f34e42b9552bbcd4c0dc675f10bab
ZayRexan <mogilevkostya@yandex.ru>
2023-04-24 21:37:22 +0700

    Вёрста и написание скрипта для плитки тегов

--- assets/js/_functions.js
+++ assets/js/_functions.js
@@ -3,3 +3,4 @@ import { select } from '../components/select/select';
 import { tabs } from '../components/tabs/tabs';
 import { swiper } from './functions/swiper-init';
 import { spollers } from '../components/spollers/spollers';
+import { showMoreTags } from './functions/showMoreTags';

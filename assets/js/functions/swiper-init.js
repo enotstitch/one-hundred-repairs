36627e69206b250df4d5c3c5b6307be1128b0d33
ZayRexan <mogilevkostya@yandex.ru>
2023-04-27 21:58:32 +0700

    Вёрстка секции с отзывами

--- assets/js/functions/swiper-init.js
+++ assets/js/functions/swiper-init.js
@@ -5,8 +5,39 @@ const swiper = new Swiper('.swiper', {
 	loop: true,
 
 	navigation: {
-		nextEl: '.swiper-btn--next',
-		prevEl: '.swiper-btn--prev',
+		nextEl: '.experts-btn--next',
+		prevEl: '.experts-btn--prev',
+	},
+
+	slidesPerView: 3,
+	spaceBetween: 40,
+
+	breakpoints: {
+		1201: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+
+		769: {
+			slidesPerView: 2,
+		},
+
+		320: {
+			allowTouchMove: true,
+			navigation: false,
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+	},
+});
+
+const swiperReviews = new Swiper('.reviews-slider', {
+	speed: 400,
+	loop: true,
+
+	navigation: {
+		nextEl: '.reviews-btn--next',
+		prevEl: '.reviews-btn--prev',
 	},
 
 	slidesPerView: 3,

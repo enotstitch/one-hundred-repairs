519d626d2c40aedbed15f48de11b60bd28a8260e
enotstitch <kessler.anna96@gmail.com>
2023-04-29 23:43:27 +0300

    Добавление маски на поля с телефонами

--- assets/js/_functions.js
+++ assets/js/_functions.js
@@ -2,6 +2,7 @@ import { header } from '../components/header/header';
 import { select } from '../components/select/select';
 import { modal } from '../components/modal/modal';
 
+import { formMask } from './functions/formMask';
 import { calc } from '../components/calc/calc';
 import { swiper } from './functions/swiper-init';
 import { tabs } from '../components/tabs/tabs';
--- assets/js/_libs.js
+++ assets/js/_libs.js
@@ -1,3 +1,4 @@
 import './libs/lazysizes.min';
+import './libs/inputmask.min';
 import fslightbox from 'fslightbox';
 import './libs/swiper-bundle.min';
